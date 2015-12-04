065810abbc2453b5a5da9bc3dcd83785854afc60
Anton Maulana <anton@microvac.co.id>
2015-12-04 14:27:56 +0700

    latihan dasar git push

---
+++ CrudMVC/Controllers/CrudController.cs
@@ -0,0 +1,127 @@
+﻿using CrudMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudMVC.Controllers
+{
+    public class CrudController : Controller
+    {
+        DB db = new DB();
+        // GET: Crud
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        // GET: Crud/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Product product = db.Products.Find(id);
+            if (product == null)
+                return HttpNotFound();
+            return View(product);
+        }
+
+        // GET: Crud/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Crud/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(product);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Crud/Edit/5
+        
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Product product = db.Products.Find(id);
+            if (product == null)
+                return HttpNotFound();
+            return View(product);
+        }
+
+        // POST: Crud/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+                return View(product);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Crud/Delete/5
+        public ActionResult Delete(int? id)
+        { 
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Product product = db.Products.Find(id);
+            if (product == null)
+                return HttpNotFound();
+            return View(product);
+        }
+
+        // POST: Crud/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int? id, Product prod)
+        {
+            try
+            {
+                Product product = new Product();
+                if (ModelState.IsValid)
+                {
+                    if (id == null)
+                        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                    product = db.Products.Find(id);
+                    if (product == null)
+                        return HttpNotFound();
+                    db.Products.Remove(product);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(product);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CrudMVC/Models/DB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Pluralization;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace CrudMVC.Models
+{
+    public class DB :DbContext
+
+    {
+        public DB() : base("DefaultConnection")
+        {
+            Database.Log = m => System.Diagnostics.Debug.WriteLine(m);
+        }
+        public virtual IDbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Types()
+            .Configure(c => c.ToTable(GetTableName(c.ClrType.Name), "public"));
+            modelBuilder.Conventions.Add<CustomKeyConvention>();
+        }
+
+        public static string GetTableName(String typeName)
+        {
+            var pluralizationService = (IPluralizationService)
+                DbConfiguration.DependencyResolver.GetService(typeof(IPluralizationService), "plural");
+
+            var result = pluralizationService.Pluralize(typeName);
+
+            result = Regex.Replace(result, ".[A-Z]", m => m.Value[0] + "_" + m.Value[1]);
+
+            return result.ToLower();
+        }
+
+        public static string GetColumnName(String typeName)
+        {
+            var result = typeName;
+
+            result = Regex.Replace(result, ".[A-Z]", m => m.Value[0] + "_" + m.Value[1]);
+
+            return result.ToLower();
+        }
+
+        public class CustomKeyConvention : Convention
+        {
+            public CustomKeyConvention()
+            {
+                Properties()
+                    .Configure(config => config.HasColumnName(DB.GetColumnName(config.ClrPropertyInfo.Name)));
+            }
+        }
+    }
+}
